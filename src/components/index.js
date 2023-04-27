b9fd27026068bb48e9d05467ee2133547b411913
Bassaamm <bassamalqrni@gmail.com>
2023-04-27 08:29:52 +0300

    flies will need to be deleted laster just for fun

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { MovieCard } from "./MovieCard";
+export {Mohcard} from "./Mohcard"
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { MovieInfo } from "./MovieInfo";
 export { MovieList } from "./MovieList";
 export { PageNotFound } from "./PageNotFound";
 export { Search } from "./Search";
+export {MohPage} from "./MohPage"
