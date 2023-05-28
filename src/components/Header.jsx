5e1bbdd7f0b9702fb16b4dd1dc98bb71ecf47d46
Bassaamm <Bassamalqrni@gmail.com>
2023-05-28 21:52:40 +0300

    fixing darkmode bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,10 +28,10 @@ export const Header = () => {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center">
-          <img
+            <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-12 mr-3"
               alt="Flowbite Logo"
