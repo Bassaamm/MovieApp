f14b1e3a33659ea95b26c0c185e797f2c8aee533
Bassaamm <Bassamalqrni@gmail.com>
2023-05-28 19:26:07 +0300

    bux deployment fixed 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
             <Link to="https://twitter.com/Znkve" className="hover:underline">
               Me™
             </Link>
-            . All Rights Reserved.
+            . All Rights Resderved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
