fd468eb4bb6faf7162fee987ffb9ad25a241fdf6
Bassaamm <Bassamalqrni@gmail.com>
2023-06-08 12:27:05 +0300

    last last verison

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ export const Header = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/" className="flex items-center">
-          <img
+            <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-12 mr-3"
               alt="Flowbite Logo"
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { MovieCard } from "./MovieCard";
-export {Mohcard} from "./Mohcard"
